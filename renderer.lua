81c64f9441dc6626ef216267f5cee2de33d063fa
ReimarPB <reimarpb@gmail.com>
2023-03-07 13:42:27 +0100

    Change design

--- renderer.lua
+++ renderer.lua
@@ -1,7 +1,38 @@
 function showFiles(files)
 	for i, file in ipairs(files) do
-		term.setCursorPos(1, i)
-		term.write(file.name)
+		showFile(files, i)
 	end
+end
+
+function showFile(files, index)
+	local file = files[index]
+
+	term.setBackgroundColor(colors.white)
+	term.setCursorPos(1, index)
+
+	-- Icon
+	local color
+	if file.isDir then
+		color = colors.yellow
+	else
+		color = colors.lightGray
+	end
+
+	term.setTextColor(colors.white)
+	term.setBackgroundColor(color)
+	term.write("\151")
+	term.setTextColor(color)
+	term.setBackgroundColor(colors.white)
+	term.write("\148")
+
+	-- Name
+	term.setTextColor(colors.black)
+	term.setBackgroundColor(colors.white)
+	term.write(file.name)
+
+	-- Whitespace
+	local x, _  = term.getCursorPos()
+	local width, _ = term.getSize()
+	term.write(string.rep(" ", width - x))
 end
 
